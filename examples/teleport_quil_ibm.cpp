fb19dfcf774f398bbde425f7e8892dc14adf6b5c
Stache <kvaltine@gmail.com>
2018-05-30 12:05:44 -0500

    update teleport examples to use quil

--- examples/teleport_quil_ibm.cpp
+++ examples/teleport_quil_ibm.cpp
@@ -33,21 +33,35 @@
 // Quantum Kernel executing teleportation of
 // qubit state to another.
 // test
-const std::string src("__qpu__ teleport (qbit qreg) {\n"
-	"   cbit creg[3];\n"
-	"   // Init qubit 0 to 1\n"
-	"   X(qreg[0]);\n"
-	"   // Now teleport...\n"
-	"   H(qreg[1]);\n"
-	"   CNOT(qreg[1],qreg[2]);\n"
-	"   CNOT(qreg[0],qreg[1]);\n"
-	"   H(qreg[0]);\n"
-	"   creg[0] = MeasZ(qreg[0]);\n"
-	"   creg[1] = MeasZ(qreg[1]);\n"
-	"   if (creg[0] == 1) Z(qreg[2]);\n"
-	"   if (creg[1] == 1) X(qreg[2]);\n"
-	"   // Check that 3rd qubit is a 1\n"
-	"   creg[2] = MeasZ(qreg[2]);\n"
+
+//old scaffold code
+//const std::string src("__qpu__ teleport (qbit qreg) {\n"
+	//"   cbit creg[3];\n"
+	//"   // Init qubit 0 to 1\n"
+	//"   X(qreg[0]);\n"
+	//"   // Now teleport...\n"
+	//"   H(qreg[1]);\n"
+	//"   CNOT(qreg[1],qreg[2]);\n"
+	//"   CNOT(qreg[0],qreg[1]);\n"
+	//"   H(qreg[0]);\n"
+	//"   creg[0] = MeasZ(qreg[0]);\n"
+	//"   creg[1] = MeasZ(qreg[1]);\n"
+	//"   if (creg[0] == 1) Z(qreg[2]);\n"
+	//"   if (creg[1] == 1) X(qreg[2]);\n"
+	//"   // Check that 3rd qubit is a 1\n"
+	//"   creg[2] = MeasZ(qreg[2]);\n"
+	//"}\n");
+	
+//written with quil
+
+const std::string src("__qpu__ teleport(AcceleratorBuffer b){\n"
+	"   X 0\n"
+	"   H 1\n"
+	"   CNOT 1 2\n"
+	"   CNOT 0 1\n"
+	"   CNOT 1 2\n"
+	"   CNOT 2 0\n"
+	"   MEASURE 2 [0]\n"
 	"}\n");
 
 int main (int argc, char** argv) {
@@ -55,9 +69,14 @@ int main (int argc, char** argv) {
 	// Initialize the XACC Framework
 	xacc::Initialize(argc, argv);
 
-	// Create a reference to the Rigetti 
-	// QPU at api.rigetti.com/qvm
-	auto qpu = xacc::getAccelerator("ibm");
+	//set the compiler to use quil
+	xacc::setCompiler("quil"); 
+
+	// Create a reference to the 
+	// QPU 
+	auto qpu = xacc::getAccelerator("rigetti");
+	//auto qpu = xacc::getAccelerator("ibm");
+	//auto qpu = xacc::getAccelerator("tnqvm");
 
 	// Allocate a register of 3 qubits
 	auto qubitReg = qpu->createBuffer("qreg", 3);
@@ -72,7 +91,24 @@ int main (int argc, char** argv) {
 	// Execute!
 	teleport(qubitReg);
 
-	qubitReg->print(std::cout);
+
+	//save the IR to text
+	//std::ofstream IRout;
+	//IRout.open("IRout.txt");
+	//program.getIR()->persist(IRout);
+	//IRout.close();
+	
+	//print out the results
+	std::vector<std::string> measurements = qubitReg->getMeasurementStrings();
+	std::cout << "getMeasurementStrings: ";
+	for(auto meas : measurements)  std::cout << meas << " ";
+	std::cout << std::endl;
+	
+	std::cout << "getExpectationValueZ: " << qubitReg->getExpectationValueZ() << std::endl;
+	
+	//print is not implemented for all accelerators it seems
+	//qubitReg->print(std::cout);
+
 
 	// Finalize the XACC Framework
 	xacc::Finalize();
